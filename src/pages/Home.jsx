58602a1821e990f5610d396668ac876dedcb40e4
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-11 16:52:58 -0400

    content added to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+      <>
+    <h1>Welcom to my Portfolio, glad you're hear!</h1>
+    <h2>Please enjoy</h2>
+      </>
+    )
   }
   
   export default Home;
