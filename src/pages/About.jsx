11c732dc56dce7e545c59f5e4c5947cbbe98a773
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-11 15:42:14 -0400

    about page set up

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from 'react'
+
 function About(props) {
-    return <h1>About</h1>;
+
+  const [about, setAbout] = useState(null)
+
+  const getAboutData = async () => {
+    const response = await fetch(props.URL + "about")
+    const data = await response.json()
+    setAbout(data)
+  }
+
+  useEffect(() => {
+    getAboutData()
+  }, [])
+
+  const loaded = () => {
+    <div> 
+      <h2>{about.name}</h2>
+      <h3>{about.email}</h3>
+      <p>{about.bio}</p>
+    </div>
+  }
+    return about ? loaded() : <h1>Please wait...</h1>
+
   }
   
   export default About;
