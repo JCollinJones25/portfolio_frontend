7685bbda39a533f711f839958f97bc1bf5e25246
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-06-16 16:26:22 -0400

    footer added and styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
 function Footer (props) {
     return (
-        <h1>Footer</h1>
+        <footer class="footer">
+        <a href="https://github.com/JCollinJones25/disclose" target="_blank" rel="noopener noreferrer">
+          <img class="github" src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="github logo"/>
+        </a>
+        <a href="https://www.linkedin.com/in/j-collin-jones/" target="_blank" rel="noopener noreferrer">
+          <img class="linkedin" src="https://cdn.freebiesupply.com/logos/large/2x/linkedin-icon-1-logo-svg-vector.svg" alt="linkedin logo"/>
+        </a>
+      </footer>
     )
 }
 
