a62414f4feb6039f78fdafa0988b2187f49c8e83
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-06-21 16:14:05 -0400

    recommendations added

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -2,7 +2,8 @@
 const Recommendations = () => {
     return (
         <div className="recommendations">
-
+            <img className="rec" src="./images/rec1.png"/>
+            <img className="rec" src="./images/rec2.png"/>
         </div>
     )
 }
