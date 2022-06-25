4d82617b3faeb78c4dc7d74dbd6f8581c31f35b3
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-06-24 22:12:54 -0400

    certificate added

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,6 +1,7 @@
 
 const Background = () => {
     return (
+        <>
         <div className="background">
             <div className="experience">
                 <img className="headshot" id="experience-headshot" src="../images/grad.png" alt="grad pic"/>
@@ -11,6 +12,11 @@ const Background = () => {
                 <p>In 2020 I graduated from the Lexington Police Academy ranked top in my class. I went on to work in law enforcement until 2022</p>
             </div>
         </div>
+        <div className="general-assembly">
+            <img src="../images/ga-cert.png" alt="ga certificate"/>
+            <p>In June of 2022 I completed a 12 week Software Engineering Immersive program at General Assembly</p>
+        </div>
+        </>
     )
 }
 
