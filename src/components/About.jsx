8b3bcedc05f2ca523b5177931eb44c2ffc30a60e
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2023-01-12 00:27:10 -0500

    update resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Links from "./Links";
-import Resume from "../JCJresume2022.pdf";
+import Resume from "../JCJ-Resume-2023.pdf";
 
 const About = (props) => {
   const [about, setAbout] = useState(null);
