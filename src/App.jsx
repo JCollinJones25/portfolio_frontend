7154a3b0c752d004943739554d8f43660c4912ee
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2023-02-06 21:57:11 -0500

    style margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Stack from "./components/Stack";
@@ -7,15 +7,14 @@ import Recommendations from "./components/Recommendations";
 import Footer from "./components/Footer";
 
 const App = () => {
-
-  const URL = 'https://collins-portfolio-spring2022.herokuapp.com/'
+  const URL = "https://collins-portfolio-spring2022.herokuapp.com/";
 
   return (
     <div className="App">
-      <About URL={URL}/>
+      <About URL={URL} />
       <h2>Projects</h2>
-      <div className='projects'>
-        <Projects URL={URL}/>
+      <div className="projects">
+        <Projects URL={URL} />
       </div>
       <h2>Stack</h2>
       <Stack />
@@ -26,6 +25,6 @@ const App = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
